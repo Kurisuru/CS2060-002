e2a55f828256823530cdaf7b09ff9f5f29b62e95
Kurisuru <emocrys101@gmail.com>
2023-02-16 21:31:41 -0700

    Update ColbertChristopherHW03.c

--- Homework/ColbertChristopherHW03.c
+++ Homework/ColbertChristopherHW03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+
 #define MIN_CHARGE_VAL 3
 #define MIN_HOURS_AT_FLAT_RATE 3
 #define MIN_HOURS_ALLOWED 0
@@ -8,7 +9,6 @@
 #define MAXIMUM_HOURS_ALLOWED 24
 #define ADDITIONAL_HOURS_RATE 0.75
 #define SENTINEL_VAL -1
-
 
 double getValidHours();
 double calculateCharge(double);
@@ -21,9 +21,12 @@ int main(void)
 	double sumCharges = 0;
 	double sumHours = 0;
 	int sumCars = 0;
-    do
+
+    do//clear buffer
     {
+        //collect user input, once the input is valid store it
         hours = getValidHours();
+        //if user input is not sentinel value perform calculations and keep looping to get user input
         if (hours != SENTINEL_VAL)
         {
             sumHours += hours;
@@ -37,6 +40,7 @@ int main(void)
     return 0;
 }
 
+//gets user data until it is valid
 double getValidHours()
 {
 	double hours = 0;
@@ -60,6 +64,7 @@ double getValidHours()
 	return hours;
 }
 
+//calculate charge based on valid data and global definitions
 double calculateCharge(double calcHours) 
 {
     double charge = 0;
@@ -71,6 +76,8 @@ double calculateCharge(double calcHours)
         charge = MAX_CHARGE_VAL;
     return charge;
 }
+
+//display a summary of totaln cars, hours, and charges
 void displaySummary(double totalHours, int totalCars, double totalCharges)
 {
         puts("Parking Garage Summary");
@@ -79,70 +86,3 @@ void displaySummary(double totalHours, int totalCars, double totalCharges)
     else
         puts("There were no cars parked today");
 }
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void guessGame(void); // function prototype
-int isCorrect(int, int); // function prototype
-
-int main(void)
-{
-    // srand( time( 0 ) ); // seed random number generator
-     //update with your name
-    puts("Hello Chris");
-    guessGame();
-
-} // end main
-
-// guessGame generates numbers between 1 and 1000
-// and checks user's guess
-void guessGame(void)
-{
-    int answer; // randomly generated number
-    int guess; // user's guess
-    int response; // 1 or 2 response to continue game
-
-    // loop until user types 2 to quit game
-    do {
-        // generate random number between 1 and 1000
-        // 1 is shift, 1000 is scaling factor
-        answer = 1 + rand() % 1000;
-
-        // prompt for guess
-        puts("I have a number between 1 and 1000.\n"
-            "Can you guess my number?\n"
-            "Please type your first guess.");
-        printf("%s", "? ");
-        scanf("%d", &guess);
-
-        // loop until correct number
-        while (!isCorrect(guess, answer))
-            scanf("%d", &guess);
-
-        // prompt for another game
-        puts("\nExcellent! You guessed the number!\n"
-            "Would you like to play again?");
-        printf("%s", "Please type ( 1=yes, 2=no )? ");
-        scanf("%d", &response);
-
-        puts("");
-    } while (response == 1);
-} // end function guessGame
-
-// isCorrect returns true if g equals a
-// if g does not equal a, displays hint
-int isCorrect(int g, int a)
-{
-    // guess is correct
-    if (g == a)
-        return 1;
-
-    // guess is incorrect; display hint
-    if (g < a)
-        printf("%s", "Too low. Try again.\n? ");
-    else
-        printf("%s", "Too high. Try again.\n? ");
-
-    return 0;
-} // end function */
