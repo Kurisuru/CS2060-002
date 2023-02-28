1aaace2b259d34e4b5edae5c6d9244a1699bef49
Kurisuru <emocrys101@gmail.com>
2023-02-28 10:43:29 -0700

    Update fig07_28.c

--- CS2060-Class-Code-main/examples/ch07/fig07_28.c
+++ CS2060-Class-Code-main/examples/ch07/fig07_28.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 // prototypes
-void function1(int a);
-void function2(int b);
-void function3(int c);
+int function1(int a);
+int function2(int b);
+int function3(int c);
 
 int main(void)
 {
    // initialize array of 3 pointers to functions that each take an
    // int argument and return void                              
-   void (*f[3])(int) = {function1, function2, function3};    
+   int (*f[3])(int) = {function1, function2, function3};    
 
    printf("%s", "Enter a number between 0 and 2, 3 to end: ");
    size_t choice; // variable to hold user's choice
@@ -24,7 +24,7 @@ int main(void)
 
       // invoke function at location choice in array f and pass
       // choice as an argument                              
-      (*f[choice])(choice);                                
+      (*f[choice])(choice);     
 
       printf("%s", "Enter a number between 0 and 2, 3 to end: ");
       //windows needs llu  mac needs lu
@@ -34,17 +34,17 @@ int main(void)
    puts("Program execution completed.");
 } 
 
-void function1(int a)
+int function1(int a)
 {
    printf("You entered %d so function1 was called\n\n", a);
 }
 
-void function2(int b)
+int function2(int b)
 {
    printf("You entered %d so function2 was called\n\n", b);
 }
 
-void function3(int c)
+int function3(int c)
 {
    printf("You entered %d so function3 was called\n\n", c);  
 }
