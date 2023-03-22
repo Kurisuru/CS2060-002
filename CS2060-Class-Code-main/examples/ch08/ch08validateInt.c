cdcb74504fbc940c5f66804a376f8f93b9722daa
Kurisuru <emocrys101@gmail.com>
2023-03-22 15:17:11 -0600

    Update ch08validateInt.c

--- CS2060-Class-Code-main/examples/ch08/ch08validateInt.c
+++ CS2060-Class-Code-main/examples/ch08/ch08validateInt.c
@@ -9,7 +9,7 @@
 
 #define LENGTH 13
 
-void  exploreValidateInt(const char* buff);
+bool exploreValidateInt(const char* buff, int* intToReturn);
 bool validateInt(char* buff, int* const validInt);
 void printLimits();
 
@@ -17,22 +17,26 @@ int main(void)
 {
 	char inputStr[LENGTH]; // create char arintray
 	size_t inputLength = 0;
+	int returnInt = 0;
 
 	printLimits();
 
 	for (unsigned int counter = 1; counter < 6; counter++)
 	{
-		puts("\nEnter an integer");
-		fgets(inputStr, LENGTH, stdin);
 
-		inputLength = strnlen(inputStr, LENGTH);
-
-		if (inputLength > 0 && inputStr[inputLength - 1] == '\n')
+		do
 		{
-			inputStr[inputLength - 1] = '\0';
-			inputLength--;
-		}
-		exploreValidateInt(inputStr);
+			
+			puts("\nEnter an integer");
+			fgets(inputStr, LENGTH, stdin);
+			inputLength = strnlen(inputStr, LENGTH);
+
+			if (inputLength > 0 && inputStr[inputLength - 1] == '\n')
+			{
+				inputStr[inputLength - 1] = '\0';
+				inputLength--;
+			}
+		} while (!exploreValidateInt(inputStr,&returnInt));
 	}
 
 }
@@ -61,29 +65,42 @@ void printLimits()
 }
 
 
-void  exploreValidateInt(const char* buff)
+bool exploreValidateInt(const char* buff, int*intToReturn)
 {
+	bool returnVal = false;
+	// declare pointer to a char that will be passed to strotol
 	char* end;
+	//The errno is set to zero at program startup and set to ERANGE if out of range when  strtol tries to convert to double
 	errno = 0;
+	//store integer when it is valid
 	int validInt = 0;
+	//perform int test on passed value, and store if it was successful
 	long intTest = strtol(buff, &end, 10);
+	//no valid integers were found within the string entered
 	if (end == buff) {
 		fprintf(stderr, "%s: not a decimal number\n", buff);
 	}
+	//string has a valid int, but contains something else after the valid int
 	else if ('\0' != *end) {
 		fprintf(stderr, "%s: extra characters at end of input: %s\n", buff, end);
 	}
+	//scanned number is either at the top or bottom value of what can be considered a long
 	else if ((LONG_MIN == intTest || LONG_MAX == intTest) && ERANGE == errno) {
 		fprintf(stderr, "%s out of range of type long\n", buff);
 	}
+	//scanned number is greater than what a long can be
 	else if (intTest > INT_MAX) {
 		fprintf(stderr, "%ld greater than INT_MAX\n", intTest);
 	}
+	//scanned number is less than what a long can be
 	else if (intTest < INT_MIN) {
 		fprintf(stderr, "%ld less than INT_MIN\n", intTest);
 	}
+	//only a valid integer was scanned 
 	else {
-		validInt = (int)intTest;
-		printf("%d is integer value ", validInt);
+		*intToReturn = (int)intTest;
+		printf("%d is integer value ", *intToReturn);
+		returnVal = true;
 	}
+	return returnVal;
 }
