bb44498e968e055e00910fc2a44e89823b436ff0
Kurisuru <emocrys101@gmail.com>
2023-04-02 16:40:36 -0600

    Update Iteration1.c

--- Homework/Iteration1.c
+++ Homework/Iteration1.c
@@ -1,5 +1,8 @@
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SIZE 80
 
@@ -19,7 +22,7 @@ typedef struct organization
 
 void setUpOrg(Organization* orgPtr);
 void donate(Organization* orgPtr);
-void generateUrl(Organization* orgPtr);
+void generateUrl(char *email[], const char *orgName[]);
 void displayInfo(const Organization* orgPtr);
 bool adminSummary(Organization* orgPtr);
 
@@ -33,21 +36,43 @@ int main(void)
 void setUpOrg(Organization* orgPtr)
 {
 	char inputStr[SIZE];
+	char* endPtr = inputStr;
 
 	puts("Enter fundraising  organization name.");
-	//fgets(orgPtr->orgName, SIZE, stdin);
+	fgets(orgPtr->orgName, SIZE, stdin);
 
 	puts("Enter fundraiser  purpose.");
+	fgets(orgPtr->purpose, SIZE, stdin);
 
 	puts("Enter first and last name.");
+	fgets(orgPtr->name, SIZE, stdin);
 
 	puts("Enter the goal amount you want to raise.");
-
+	fgets(inputStr, SIZE, stdin);
+	orgPtr->goalAmount = strtod(inputStr, &endPtr);
+	//needs validation
+	 
 	puts("Enter email address.");
+	fgets(orgPtr->email, SIZE, stdin);
 
 	puts("Enter password.");
+	fgets(orgPtr->password, SIZE, stdin);
 
+	generateUrl(&(orgPtr->url), &(orgPtr->orgName));
+	orgPtr->totalDonationAmount = 0;
+	orgPtr->totalDonors = 0;
+	orgPtr->totalProcessingAmount = 0;
 
+	displayInfo(orgPtr);
+
+}
+
+char fgetsNoNewLine(char *str, int size, FILE *stream)
+{
+	if (fgets(str, size, stream) != NULL)
+	{
+
+	}
 }
 
 void donate(Organization* orgPtr)
@@ -55,14 +80,30 @@ void donate(Organization* orgPtr)
 
 }
 
-void generateUrl(Organization* orgPtr)
+void generateUrl(char *email[], const char* orgName[])
 {
+	/**email = "https:donate.com/";
+	char* bufferPtr;
 
+	bufferPtr = strchr(*orgName, ' ');
+
+	while (bufferPtr != NULL)
+	{
+		*bufferPtr = '-';
+		bufferPtr = strchr(bufferPtr, ' ');
+	}*/
+	char* orgNameWithDashes[SIZE];
+	strncpy(*orgNameWithDashes, *orgName, sizeof(orgNameWithDashes));
+
+	char* tokenPtr = strtok(orgName, " ");
+
+	//strtok
 }
 
 void displayInfo(const Organization* orgPtr)
 {
-
+	printf("Thank you %s. The url to raise funds for %s is %s.", 
+		orgPtr->name, orgPtr->orgName, orgPtr->url);
 }
 
 bool adminSummary(Organization* orgPtr)
@@ -97,5 +138,3 @@ bool validatePassword(const char password[])
 
 	return isValid;
 }
-
-//fgets
